353e6966717c3616895e7af27cb190544451cb52
Marianolpz11 <marianoalopez11@gmail.com>
2025-05-07 14:47:18 -0400

    1#07-05-25

---
+++ Vibracion.py
@@ -0,0 +1,24 @@
+import math
+
+def calcular_ppv(K, l, ro, H, xs, xo, alpha):
+    """
+    Calcula la velocidad pico de partícula (PPV) usando el modelo de Holmberg-Persson.
+
+    Parámetros:
+    - K (float): Constante empírica.
+    - l (float): Longitud total de la columna explosiva.
+    - ro (float): Distancia horizontal entre el pozo y el receptor.
+    - H (float): Altura del pozo cargado.
+    - xs (float): Profundidad donde comienza la carga explosiva (x_s).
+    - xo (float): Coordenada vertical del receptor (x_o).
+    - alpha (float): Exponente de atenuación.
+
+    Retorna:
+    - PPV (float): Velocidad pico de partícula estimada.
+    """
+    term1 = l / ro
+    angle1 = math.atan((H + xs - xo) / ro)
+    angle2 = math.atan((xo - xs) / ro)
+    suma_angulos = angle1 + angle2
+    ppv = K * (term1 * suma_angulos) ** alpha
+    return ppv
